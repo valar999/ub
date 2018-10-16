bc9f466a26c59f613be6884449063ebcfe0ae290
Oleksiy Pyltsov <valar999@gmail.com>
2018-10-16 18:08:15 +0300

    added stats output, sync staff changes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"math"
 	"net"
 	"os"
@@ -22,24 +23,31 @@ const ConnectTimeout = time.Second * 3
 var addr string
 var numConns int
 var statsChan chan Stat = make(chan Stat)
+var startWG sync.WaitGroup
 
 func worker(ctx context.Context) {
+	startWG.Done()
 	for {
 		stat := Stat{}
 		dialer := new(net.Dialer)
 		dialer.Timeout = ConnectTimeout
 		start := time.Now()
 		conn, err := dialer.DialContext(ctx, "tcp", addr)
-		if err != nil {
-			stat.err = err
-		} else {
+		if err == nil {
 			stat.connect = time.Since(start)
 			go conn.Close()
+		} else {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+			stat.err = err
 		}
 		select {
-		case statsChan <- stat:
 		case <-ctx.Done():
 			return
+		case statsChan <- stat:
 		}
 	}
 }
@@ -68,7 +76,7 @@ func showStat(stats []Stat, errs []Stat, duration time.Duration) {
 		stdConnect = time.Duration(math.Sqrt(
 			(stdConnectFloat / (float64(n) - 1))))
 	}
-	reqPerSec := float64(n) / duration.Seconds()
+	reqPerSec := float64(len(stats)) / duration.Seconds()
 	fmt.Printf("Running %v test of %v\n", duration, addr)
 	fmt.Printf("%v concurrent connections\n", numConns)
 	fmt.Printf("\n")
@@ -82,8 +90,27 @@ func showStat(stats []Stat, errs []Stat, duration time.Duration) {
 	if len(errs) > 0 {
 		fmt.Printf("Errors: %v (%.1f%%)\n", len(errs),
 			float64(len(errs))/float64(len(stats)+len(errs))*100)
+		errTypes := make(map[string]int)
+		for _, stat := range errs {
+			var s string
+			switch t := stat.err.(type) {
+			//case *net.OpError:
+			//	s = t.Err
+			default:
+				s = t.Error()
+			}
+			errTypes[s]++
+		}
+		for err, n := range errTypes {
+			s := err
+			switch err {
+			case "dial":
+				s = "connect"
+			}
+			fmt.Printf("  %v\t%v\n", n, s)
+		}
 	}
-	fmt.Printf("Req/sec: %v\n", reqPerSec)
+	fmt.Printf("Req/sec: %.1f\n", reqPerSec)
 }
 
 func setLimits(nofile uint64) {
@@ -98,6 +125,7 @@ func setLimits(nofile uint64) {
 
 func main() {
 	setLimits(100000)
+	log.SetFlags(log.Lmicroseconds)
 	var durationInt int
 	flag.IntVar(&numConns, "c", 1, "connections to keep open")
 	flag.IntVar(&durationInt, "d", 10, "duration of test")
@@ -106,15 +134,16 @@ func main() {
 	addr = flag.Arg(0)
 	duration := time.Second * time.Duration(durationInt)
 
-	conn, err := net.Dial("tcp", addr)
+	conn, err := net.DialTimeout("tcp", addr, ConnectTimeout)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	conn.Close()
 
-	stats := make([]Stat, 0, numConns)
+	stats := make([]Stat, 0, numConns*int(duration.Seconds()))
 	errs := make([]Stat, 0)
+	stopChan := make(chan bool)
 	go func() {
 		for {
 			select {
@@ -124,18 +153,26 @@ func main() {
 				} else {
 					errs = append(errs, stat)
 				}
+			case <-stopChan:
+				return
 			}
 		}
 	}()
 	var wg sync.WaitGroup
 	wg.Add(numConns)
-	ctx, _ := context.WithTimeout(context.Background(), duration)
+	startWG.Add(numConns)
+	ctx, cancel := context.WithCancel(context.Background())
 	for i := 0; i < numConns; i++ {
 		go func() {
 			worker(ctx)
 			wg.Done()
 		}()
 	}
+	startWG.Wait()
+	time.Sleep(duration)
+	stopChan <- true
+	//time.Sleep(time.Millisecond * 100)
+	cancel()
 	wg.Wait()
 	showStat(stats, errs, duration)
 }
